db5b2db0933165a72efca5f873182ed9d021cf92
Tyler Twining <twijn@twijn.net>
2024-05-21 15:22:28 -0400

    Allow setting of Discord role colors

--- express/controllers/management/updateRoles.js
+++ express/controllers/management/updateRoles.js
@@ -11,6 +11,12 @@ router.post("/", async (req, res) => {
     const roles = await DiscordRole.find({});
     for (let i = 0; i < roles.length; i++) {
         roles[i].settings.roleAdd = body.roleAdd.hasOwnProperty(roles[i]._id);
+        if (body.wheelColor.hasOwnProperty(roles[i]._id)) {
+            roles[i].settings.wheelColor = body.wheelColor[roles[i]._id];
+        }
+        if (body.wheelTextColor.hasOwnProperty(roles[i]._id)) {
+            roles[i].settings.wheelTextColor = body.wheelTextColor[roles[i]._id];
+        }
         await roles[i].save();
     }
     res.redirect("/?info=Role+settings+updated!");
