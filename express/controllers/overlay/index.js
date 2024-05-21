472a9702eb636e5b943a8059cb2fbab3ed3dcaaf
Tyler Twining <twijn@twijn.net>
2024-05-21 15:23:07 -0400

    Send settings to overlay

--- express/controllers/overlay/index.js
+++ express/controllers/overlay/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const fetcher = require("../../../twitch/emotes");
+const utils = require("../../../utils");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const settings = await utils.settings.get();
     res.render("pages/overlay", {
         urls: fetcher.emotes.map(x => x.toLink(2)),
+        settings,
     });
 });
 
